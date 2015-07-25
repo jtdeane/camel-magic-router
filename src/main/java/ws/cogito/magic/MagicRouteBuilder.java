1a36d0ed70bb5e0dafe37b4aa280029f0f355eb3
Jeremy Deane <jeremy.deane@gmail.com>
2015-07-25 17:37:04 -0400

    Included some addition routes

--- src/main/java/ws/cogito/magic/MagicRouteBuilder.java
+++ src/main/java/ws/cogito/magic/MagicRouteBuilder.java
@@ -1,10 +1,7 @@
 package ws.cogito.magic;
 
-import org.apache.camel.Exchange;
-import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.builder.xml.XPathBuilder;
-import org.apache.camel.dataformat.xmljson.XmlJsonDataFormat;
 
 /**
  * Magic Route Builder
@@ -33,47 +30,12 @@ public class MagicRouteBuilder extends RouteBuilder {
 
     	/**
     	 * Content Based Routing - simple expression - V1
-    	 
+    	 */
     	from("activemq:emagic.order").
     	choice().
     		when().simple("${in.body} contains 'Houdini'").
     			to("activemq:priority.order").
     		otherwise().
-    			to("activemq:magic.order");*/
-    	/**
-    	 * Content Based Routing - Mediation, simple expression
-		
-		XmlJsonDataFormat xmlJsonFormat = new XmlJsonDataFormat();
-		xmlJsonFormat.setForceTopLevelObject(true);
-
-    	from("activemq:emagic.order").
-    	choice().
-    		when().simple("${in.body} contains 'Houdini'").
-				process(new Processor() {
-	        		public void process(Exchange exchange) {	
-	        			exchange.getIn().setHeader("VIP", "true");
-	        		}
-	        	}).
-    			to("activemq:priority.order").
-    		otherwise().
-    			marshal(xmlJsonFormat).
-    			transform(body().regexReplaceAll("@", "")).
-    			to("activemq:magic.order");*/
-    	
-    	
-    	/**
-    	 * Content Based Routing - Wire-Tap to ActiveMQ Topic 
-    	  	
-    	from("activemq:emagic.order").
-    		wireTap("direct:ministry").
-    	to("activemq:magic.order");
-    	
-    	from("direct:ministry").
-			choice().
-				when().simple("${in.body} contains 'Elder Wand'").
-					log("ILLEGAL MAGIC ALERT").
-					to("activemq:topic:magic.alerts").		
-				otherwise().
-					log("...off into the ether");*/
+    			to("activemq:magic.order");
     }
 }
